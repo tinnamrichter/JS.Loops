6aab0282efde5b4eaebe5b26e6d5cbf704ae56e5
tinnamrichter <tinn0229@easv365.dk>
2019-09-16 13:22:10 +0200

    tilfojelser

--- jsLoops72.js
+++ jsLoops72.js
@@ -1,11 +1,14 @@
 'use strict';
-var a = Number(prompt('What is the square root of'));
-var squareRoot = function(x) {
-    if () {
 
-    } else {
+var squareRoot = function(a) { //x1
+    let d = 6;
+    let c = b;
+    let b = a / 2; //x0
 
-    }
+while (a <= 1) {
+    b = (c + a / b) /2);
 }
+};
 
-return (y + (a/y)/2);
+//var a = Number(prompt('What is the square root of'));
+console.log(squareRoot(9));
---
+++ jsPlay75.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const roll = function(a) {
+    return Math.floor(Math.random() * a + 1);
+}
+
+const times = function(b, a) {
+    let res;
+    let tally = [];
+
+    while (i < a) {
+        res = roll(a);
+        tally[i] = 0; 
+        i++;
+    }
+};
+
+var a = 6;
+var b = 2;
+let i = 1;
+console.log('der blev slået' + tally[i];

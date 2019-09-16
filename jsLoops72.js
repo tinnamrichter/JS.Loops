7d2a07bd58001447dc46ac0dc27a8b46b7bb37d9
tinnamrichter <tinn0229@easv365.dk>
2019-09-16 15:00:43 +0200

    rettelser

--- jsLoops72.js
+++ jsLoops72.js
@@ -7,7 +7,7 @@ var squareRoot = function(a) { //x1
 
 
 
-while (Math.abs((x1 - x2) >= 0.0001) || Math.abs((x2 - x3) >= 0.0001) ) {
+while (Math.abs(x1 - x2) >= 0.0001 || Math.abs(x2 - x3) >= 0.0001) {
     x1 = x2;
     x2 = x3;
     x3 = ((x2 + a/x2) /2);
